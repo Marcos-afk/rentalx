dcee40e8fe2fc0b97cdf6bde7dc45f5c94ca96d5
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-09-25 23:25:19 -0300

    initial settings

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Servidor rodando na url/porta: ${PORT}`);
+});
