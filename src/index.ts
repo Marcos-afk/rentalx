f3e6b7f269afa7dbbfad76b3e9559493aa83a92c
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-09-27 10:01:34 -0300

    module layer implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
-import { categoriesRoutes } from './routes/categories.router';
+import { categoriesRoutes } from './modules/cars/routes/categories.router';
 
 config();
 
