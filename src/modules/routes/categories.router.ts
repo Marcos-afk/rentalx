0640e1d43df3dd20d77a62a4f47136a98209fcbc
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-09-29 15:28:12 -0300

    upload route implementation

--- src/modules/routes/categories.router.ts
+++ src/modules/routes/categories.router.ts
@@ -1,8 +1,12 @@
 import { Router, Request, Response } from 'express';
 import { createCategoryController } from '../cars/useCases/createCategory';
 import { listCategoriesController } from '../cars/useCases/listCategories';
+import multer from 'multer';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.get('/', (req: Request, res: Response) => {
   return listCategoriesController.handle(req, res);
@@ -10,6 +14,11 @@ categoriesRoutes.get('/', (req: Request, res: Response) => {
 
 categoriesRoutes.post('/', (req: Request, res: Response) => {
   return createCategoryController.handle(req, res);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (req: Request, res: Response) => {
+  const { file } = req;
+  return res.status(200).json(file);
 });
 
 export { categoriesRoutes };
