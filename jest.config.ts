98da5bc29a6e675226e5a82bad6399a7ee56bf63
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-10-10 09:32:50 -0300

    jest initial settings

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageProvider: 'v8',
+  clearMocks: true,
+  bail: true,
+};
---
+++ src/modules/cars/useCases/createCategory/CreateCategory.test.ts
@@ -0,0 +1,6 @@
+describe('Create category', () => {
+  it('test', () => {
+    const sum = 1 + 1;
+    expect(sum).toEqual(2);
+  });
+});
